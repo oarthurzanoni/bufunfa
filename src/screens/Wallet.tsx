89664b7c4ffdf8fcd9440e9b8f8dcd163422c39e
Mitacho <arthurf@tuta.io>
2021-06-06 16:05:40 -0300

    New options have been added to the Wallet screen.

--- src/screens/Wallet.tsx
+++ src/screens/Wallet.tsx
@@ -3,19 +3,71 @@ import React from "react";
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  TouchableWithoutFeedback
 } from "react-native";
 
-import { StackScreenProps } from "@react-navigation/stack";
+import Icon from "../components/Icon";
 
-import { StackParamList } from "../types/Navigator";
+import Wallet from "../assets/images/svgs/Wallet";
+import Bloom from "../assets/images/svgs/Bloom";
 
-type Props = StackScreenProps<StackParamList, "Wallet">;
-
-export default function WalletScreen({ navigation }: Props): JSX.Element {
+export default function WalletScreen(): JSX.Element {
   return(
     <View style={styles.container}>
-      <Text>Wallet</Text>
+      <View style={styles.paddingView} />
+      <TouchableWithoutFeedback
+        onPress={() => console.log("Button pressed!")}
+      >
+        <View style={styles.card}>
+          <View style={styles.image}>
+            <Icon
+              fill="#050505"
+              height="37px"
+              width="37px"
+              svg={Wallet}
+            />
+          </View>
+          <View style={styles.description}>
+            <Text style={[ styles.text ]}>Fechar o mês</Text>
+            <View style={{ flex: 1 }}>
+              <Text
+                numberOfLines={1}
+                style={[
+                  styles.text,
+                  styles.textAmout
+                ]}
+              >R$ 1.441,10</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={() => console.log("Button pressed!")}
+      >
+        <View style={styles.card}>
+          <View style={styles.image}>
+            <Icon
+              fill="#050505"
+              height="37px"
+              width="37px"
+              svg={Bloom}
+            />
+          </View>
+          <View style={styles.description}>
+            <Text style={[ styles.text ]}>Limpar tudo</Text>
+            <View style={{ flex: 1 }}>
+              <Text
+                numberOfLines={1}
+                style={[
+                  styles.text,
+                  styles.textAmout
+                ]}
+              >R$ 0,00</Text>
+            </View>
+          </View>
+        </View>
+      </ TouchableWithoutFeedback>
     </View>
   );
 }
@@ -27,5 +79,49 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
 
     backgroundColor: "#ffffff",
+  },
+
+  image: {
+    marginRight: 20,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  card: {
+    height: 42,
+
+    flexDirection: "row",
+
+    marginTop: 10,
+    marginBottom: 24,
+  },
+
+  text: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 16,
+
+    textAlignVertical: "center",
+
+    color: "#050505",
+  },
+
+  textAmout: {
+    flex: 1,
+
+    textAlign: "right",
+
+    color: "#70B657"
+  },
+
+  description: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  
+  paddingView: {
+    height: 45
   }
 });
