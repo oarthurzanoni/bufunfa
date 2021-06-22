66130245c2f95dd8ba96fa059d4f39fd6cfccb9c
Mitacho <arthurf@tuta.io>
2021-06-22 16:34:15 -0300

    Add new categories and icons.

--- src/settings/CategoryOptions.ts
+++ src/settings/CategoryOptions.ts
@@ -9,11 +9,57 @@ import TakeAwayFood from "../assets/images/svgs/TakeAwayFood";
 import OnlineStore from "../assets/images/svgs/OnlineStore";
 import TShirt from "../assets/images/svgs/TShirt";
 import Car from "../assets/images/svgs/Car";
+import Coconut from "../assets/images/svgs/Coconut";
+import GlassOfWine from "../assets/images/svgs/GlassOfWine";
+import Restaurant from "../assets/images/svgs/Restaurant";
+import Food from "../assets/images/svgs/Food";
+import Donut from "../assets/images/svgs/Donut";
+import Gasoline from "../assets/images/svgs/Gasoline";
+import Maintenance from "../assets/images/svgs/Maintenance";
+import Scissors from "../assets/images/svgs/Scissors";
+import Haircut from "../assets/images/svgs/Haircut";
+import Pets from "../assets/images/svgs/Pets";
+import Donation from "../assets/images/svgs/Donation";
+import Fitness from "../assets/images/svgs/Fitness";
+import Train from "../assets/images/svgs/Train";
+import Plane from "../assets/images/svgs/Plane";
 
 const CategoryOptions: IOption[] = [
   {
     icon: Cloud,
     description: "Outros"
+  },
+  {
+    icon: Food,
+    description: "Alimentação"
+  },
+  {
+    icon: Plane,
+    description: "Viagem"
+  },
+  {
+    icon: Train,
+    description: "Transporte"
+  },
+  {
+    icon: Fitness,
+    description: "Saúde"
+  },
+  {
+    icon: Donut,
+    description: "Padaria"
+  },
+  {
+    icon: Coconut,
+    description: "Praia"
+  },
+  {
+    icon: GlassOfWine,
+    description: "Bares"
+  },
+  {
+    icon: Restaurant,
+    description: "Restaurante"
   },
   {
     icon: House,
@@ -29,7 +75,31 @@ const CategoryOptions: IOption[] = [
   },
   {
     icon: PottedPlant,
-    description: "Residência"
+    description: "Do lar"
+  },
+  {
+    icon: Donation,
+    description: "Caridade"
+  },
+  {
+    icon: Pets,
+    description: "Animais"
+  },
+  {
+    icon: Haircut,
+    description: "Salão de beleza"
+  },
+  {
+    icon: Scissors,
+    description: "Barbearia"
+  },
+  {
+    icon: Maintenance,
+    description: "Reparos de eletrônicos"
+  },
+  {
+    icon: Gasoline,
+    description: "Combustível"
   },
   {
     icon: TakeAwayFood,
