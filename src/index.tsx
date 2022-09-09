c5e52927b80509af3cf951c336024f8b9cef7cae
Mitacho <arthurf@tuta.io>
2022-09-09 07:05:14 -0300

    feat: check if app is loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,41 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen, ProfileScreen } from "./screens";
+import { StatusBar } from "expo-status-bar";
+import { useTheme, useUser } from "hooks";
+import { HomeScreen, ProfileScreen } from "navigation";
+import "./translations/i18n";
 
-import type { RootStackParamList } from "./screens/types";
+import type { RootStackParamList } from "navigation";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export function Main() {
+  const { isLoadingUser } = useUser();
+  const { theme, isLoadingTheme } = useTheme();
+
+  const isAppLoading = isLoadingTheme || isLoadingUser;
+
+  if (isAppLoading) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          animation: "fade_from_bottom",
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar translucent={true} style={theme.statusBar.style} />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            animation: "fade_from_bottom",
+          }}
+        >
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
